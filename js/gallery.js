10345c91b0c7dd2631d307cf857aa2ca4f5a4296
Jean Juliano <jeanjuliano927@gmail.com>
2016-02-28 17:24:51 -0800

    added js files

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+var cakeBox = document.getElementsByClassName("cupcake-photo");
+
+cakeBox.addEventListener("click", expandCake);
+
+function expandCake() {
+    //when the cake box is clicked, expand to a larger box
+    alert("hallelujah!");
+};
+    
+    
