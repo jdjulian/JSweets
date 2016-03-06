60ee502ac60d67896fc8e3c7e768148c3fb21a4d
Jean Juliano <jeanjuliano927@gmail.com>
2016-03-05 20:09:53 -0800

    modified gallery page

--- js/gallery.js
+++ js/gallery.js
@@ -1,52 +1,44 @@
-var cupcakeBox = document.getElementById("cupcake-photo");
-var cupcakes = document.getElementById("cupcakes"); 
-
-var cakeBox = document.getElementById("cake-photo");
-var cakes = document.getElementById("cakes");
-
-var cakePopBox = document.getElementById("cakepop-photo");
-var cakePops = document.getElementById("cake-pops");
-var otherSweetsBox = document.getElementById("other-photo");
-var otherSweets = document.getElementById("other-sweets");
-
-var cupcakeSlide = document.getElementById("cupcake-photo").addEventListener("click", function showCupcakeSlide() {
-	cupcakeBox.style.width = "800px";
-    cupcakeBox.style.height = "500px";
-    cupcakes.style.visibility = "hidden";
-    cakeBox.style.visibility = "hidden";
-    cakePopBox.style.visibility = "hidden";
-    otherSweetsBox.style.visibility = "hidden";
-
-});
-
-var cakeSlide = document.getElementById("cake-photo").addEventListener("click", function showCakeSlide() {
-	cakeBox.style.width = "800px";
-    cakeBox.style.height = "500px";
-    cakes.style.display = "none";
-    cupcakeBox.style.display = "none";
-    cakePopBox.style.display = "none";
-    otherSweetsBox.style.display = "none";
-    
-});
-
-var otherSweetSlide = document.getElementById("other-photo").addEventListener("click", function showOtherSlide() {
-	otherSweetsBox.style.width = "800px";
-    otherSweetsBox.style.height = "500px";
-    otherSweets.style.display = "none";
-    cupcakeBox.style.display = "none";
-    cakePopBox.style.display = "none";
-    cakeBox.style.display = "none";   
-});
-
-var cakePopSlide = document.getElementById("cakepop-photo").addEventListener("click", function cakePopSlide() {
-	cakePopBox.style.width = "800px";
-    cakePopBox.style.height = "500px";
-    cakePops.style.display = "none";
-    cupcakeBox.style.display = "none";
-    otherSweetsBox.style.display = "none";
-    cakeBox.style.display = "none";   
-});
-                                                    
+        $("#cupcake-photo").on('click', function(){
+             $("#cupcake-slider").find('.gallery').css('display', 'block');
+             $("#cake-photo").css('display', 'none');
+             $("#cakepop-photo").css('display', 'none');
+             $("#other-photo").css('display', 'none');
+             $("#cupcake-photo").css('display', 'none');
+         });
+         
+         $("#cake-photo").on('click', function(){
+             $("#cake-slider").find('.gallery').css('display', 'block');
+             $("#cake-photo").css('display', 'none');
+             $("#cakepop-photo").css('display', 'none');
+             $("#other-photo").css('display', 'none');
+             $("#cupcake-photo").css('display', 'none');
+         });
+         
+         $("#other-photo").on('click', function(){
+             $("#other-slider").find('.gallery').css('display', 'block');
+             $("#cake-photo").css('display', 'none');
+             $("#cakepop-photo").css('display', 'none');
+             $("#other-photo").css('display', 'none');
+             $("#cupcake-photo").css('display', 'none');
+         });
+         
+         $("#cakepop-photo").on('click', function(){
+             $("#cakepop-slider").find('.gallery').css('display', 'block');
+             $("#cake-photo").css('display', 'none');
+             $("#cakepop-photo").css('display', 'none');
+             $("#other-photo").css('display', 'none');
+             $("#cupcake-photo").css('display', 'none');
+         });
+         
+         $(function($) {
+             $('.slider').sss({
+                 slideShow : true, // Set to false to prevent SSS from automatically animating.
+                 startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
+                 transition : 400, // Length (in milliseconds) of the fade transition.
+                 speed : 3500, // Slideshow speed in milliseconds.
+                 showNav : true // Set to false to hide navigation arrows.
+             });
+         });                                                
 
     
     
